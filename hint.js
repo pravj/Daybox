ece01a9eb134e32d47a77fe848e3667d70f94c38
Pravendra Singh <hackpravj@gmail.com>
2013-01-31 10:39:45 -0800

    Create hint.js

---
+++ hint.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+ $("#a").click(function() {
+    $("#c").html('<input type="text" name="j" placeholder="   MM/DD [DOB format]" width="250px;"/>');});
+ $("#b").click(function() {
+    $("#c").html('<input type="text" name="j" placeholder="       Enter First Name" width="250px;"/>');});
+});
